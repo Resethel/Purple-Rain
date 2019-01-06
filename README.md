c2bf30282d71be77b60a3287f7613c0e7635770e
Resethel <resethel@gmail.com>
2019-01-06 23:42:01 +0100

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# Purple-Rain
+# Purple Rain
+
+>This C++ program creates  a canvas based particle system to simulate rainfall.
+>You know... Purple Rain ~ ♪.
+
+
+## Building
+
+Requires the SFML library
+
+On Ubuntu:
+
+```sh
+sudo apt-get install libsfml-dev
+cmake <path-to-source>
+make
+```
+
+On OSX with [Homebrew](https://brew.sh/):
+```sh
+brew install sfml
+cmake <path-to-source>
+make
+```
+
+## Run
+```sh
+./Purple-Rain
+```
+
+---
+## RoadMap
+	[x] Create a canvas with random number of particles spaced at random intervals that animate down through the height of the canvas
+	[x] For every fame of the animation, update the number of rain drops to create a continuous, infinite rainfall
+	[ ] Add a UI to allow users to control the
+		[ ] start
+		[ ] stop
+		[ ] reset
+		[ ] speed
+		[ ] density
+    [ ] Add some wind to blow the rain diagonally
+    [ ] As the rain reaches the bottom of the canvas, have them form a puddle which fills the screen evenly
--- src/Rain/Canvas.cpp
+++ src/Rain/Canvas.cpp
@@ -15,7 +15,7 @@ namespace rain
     , mGravity(12)
     {
 
-        for(int d(0) ; d < mDropAmount ; d++)
+        for(int d(0) ; d < 500 ; d++)
         {
             this->addRainDrop();
         }
@@ -27,25 +27,28 @@ namespace rain
 
     void Canvas::addRainDrop()
     {
-        Drop::Ptr dr(new Drop(Rd.rand(1,800), -Rd.rand(100,300), Rd.rand(1,20)));
+        Drop::Ptr dr(new Drop(Rd.rand(1,800), -Rd.rand(100
+            ,300), Rd.rand(1,20)));
         mDropArray.push_back(std::move(dr));
     }
 
 
     void Canvas::update(sf::Time dt)
     {
-        for(int d(0) ; d < mDropAmount ; d++)
+        for(int d(0) ; d < mDropArray.size() ; d++)
         {
             mDropArray[d]->fall(mGravity, dt);
 
             if(mDropArray[d]->getPosition().y > 600.f)
             {
                 mDropArray.erase(mDropArray.begin() + d);
-                this->addRainDrop();
+                //this->addRainDrop();
 
             }
 
         }
+
+        this->addRainDrop();
 
     }
 
